c781c5abfd47b74b6a8b2cb96c864745ccad3e2c
AlexMcDermott <alexjmcd2202@gmail.com>
2024-01-21 19:45:01 +1000

    refactor: :memo: update title and removed description after portfolio clone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -154,8 +154,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Alex McDermott</title>
-        <meta name="description" content="Welcome to my portfolio" />
+        <title>Seam Carving</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className="flex justify-center">
